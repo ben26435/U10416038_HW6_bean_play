655037bbd897bab3fc4bdca9cb54c53ae79290af
ben26435 <ben26435@gmail.com>
2016-04-24 13:16:47 +0800

    Update BeanGame.java

--- BeanGame.java
+++ BeanGame.java
@@ -44,87 +44,92 @@ public class BeanGame extends Application{//U10416038 °¨¹Å¨}
 			}
 			pane.getChildren().add(circle[i]);
 		}
+	
 		Button btPlay = new Button("Play");//Button to allow user to start the Game
-        btPlay.setLayoutX(180d);
-        btPlay.setLayoutY(360d);
-        btPlay.setPrefSize(100, 20);
-        pane.getChildren().add(btPlay);
-        Label counter = new Label("You have thrown "+count+" beans");//Label to show How many bean you have thrown
-        counter.setLayoutX(0);
-        counter.setLayoutY(0);
-        pane.getChildren().add(counter);
+        	btPlay.setLayoutX(180d);
+		btPlay.setLayoutY(360d);
+        	btPlay.setPrefSize(100, 20);
+        	pane.getChildren().add(btPlay);
+        
+        	Label counter = new Label("You have thrown "+count+" beans");//Label to show How many bean you have thrown
+        	counter.setLayoutX(0);
+        	counter.setLayoutY(0);
+        	pane.getChildren().add(counter);
+		
 		Label eachCounter[] = new Label[8];//Labels to show How many balls in each part of the bottom
-        int[] eachCount = new int[8];
-        for(int i = 0;i<8;i++) {
-            eachCount[i] = 0;
-            eachCounter[i] = new Label(String.valueOf(eachCount[i]));
-            eachCounter[i].setLayoutX(120 + i*30);
-            eachCounter[i].setLayoutY(330);
-            pane.getChildren().add(eachCounter[i]);
-        }
+        	int[] eachCount = new int[8];
+        	for(int i = 0;i<8;i++) {
+			eachCount[i] = 0;
+			eachCounter[i] = new Label(String.valueOf(eachCount[i]));
+			eachCounter[i].setLayoutX(120 + i*30);
+			eachCounter[i].setLayoutY(330);
+			pane.getChildren().add(eachCounter[i]);
+        	}
+		
 		btPlay.setOnAction(new EventHandler<ActionEvent>() {//Button Function
-             @Override // Override the handle method
-            public void handle(ActionEvent e) {
-                Timeline timeline = new Timeline();
-                SecureRandom srand = new SecureRandom();
-                int[] change=new int[7];
-                for(int i = 0;i<7;++i) {
-                       change[i] = (srand.nextInt(2)==0) ? 1 : -1 ;
-                }
-                Circle circle = new Circle(230d,50d,5);             
-                circle.setFill(Color.rgb(srand.nextInt(256), srand.nextInt(256), srand.nextInt(256)));
-                pane.getChildren().add(circle);
-                timeline.getKeyFrames().addAll(
-                        new KeyFrame(new Duration(300),
-                          new KeyValue(circle.translateXProperty(), 0),
-                          new KeyValue(circle.translateYProperty(), 55)),
-                        new KeyFrame(new Duration(450),
-                          new KeyValue(circle.translateXProperty(), change[0]*9),
-                          new KeyValue(circle.translateYProperty(), 85)),
-                        new KeyFrame(new Duration(600),
-                          new KeyValue(circle.translateXProperty(), change[0]*15),
-                          new KeyValue(circle.translateYProperty(), 85)),
-                        new KeyFrame(new Duration(750),
-                          new KeyValue(circle.translateXProperty(), change[0]*15+change[1]*9),
-                          new KeyValue(circle.translateYProperty(), 85)),
-                        new KeyFrame(new Duration(900),
-                          new KeyValue(circle.translateXProperty(), change[0]*15+change[1]*9),
-                          new KeyValue(circle.translateYProperty(), 115)),
-                        new KeyFrame(new Duration(1050),
-                          new KeyValue(circle.translateXProperty(), change[0]*15+change[1]*15+change[2]*9),
-                          new KeyValue(circle.translateYProperty(), 115)),
-                        new KeyFrame(new Duration(1200),
-                          new KeyValue(circle.translateXProperty(), change[0]*15+change[1]*15+change[2]*15),
-                          new KeyValue(circle.translateYProperty(), 145)),
-                        new KeyFrame(new Duration(1350),
-                          new KeyValue(circle.translateXProperty(), change[0]*15+change[1]*15+change[2]*15+change[3]*9),
-                          new KeyValue(circle.translateYProperty(), 145)),
-                        new KeyFrame(new Duration(1500),
-                          new KeyValue(circle.translateXProperty(), change[0]*15+change[1]*15+change[2]*15+change[3]*15),
-                          new KeyValue(circle.translateYProperty(), 175)),
-                        new KeyFrame(new Duration(1650),
-                          new KeyValue(circle.translateXProperty(), change[0]*15+change[1]*15+change[2]*15+change[3]*15+change[4]*9),
-                          new KeyValue(circle.translateYProperty(), 175)),
-                        new KeyFrame(new Duration(1800),
-                          new KeyValue(circle.translateXProperty(), change[0]*15+change[1]*15+change[2]*15+change[3]*15+change[4]*15),
-                          new KeyValue(circle.translateYProperty(), 205)),
-                        new KeyFrame(new Duration(1950),
-                          new KeyValue(circle.translateXProperty(), change[0]*15+change[1]*15+change[2]*15+change[3]*15+change[4]*15+change[5]*9),
-                          new KeyValue(circle.translateYProperty(), 205)), 
-                        new KeyFrame(new Duration(2100),
-                          new KeyValue(circle.translateXProperty(), change[0]*15+change[1]*15+change[2]*15+change[3]*15+change[4]*15+change[5]*15),
-                          new KeyValue(circle.translateYProperty(), 235)),
-                        new KeyFrame(new Duration(2250),
-                          new KeyValue(circle.translateXProperty(), change[0]*15+change[1]*15+change[2]*15+change[3]*15
+		@Override // Override the handle method
+		public void handle(ActionEvent e) {
+			Timeline timeline = new Timeline();
+                	SecureRandom srand = new SecureRandom();
+                	int[] change=new int[7];
+                	for(int i = 0;i<7;++i) {
+				change[i] = (srand.nextInt(2)==0) ? 1 : -1 ;
+                	}
+                	Circle circle = new Circle(230d,50d,5);             
+                	circle.setFill(Color.rgb(srand.nextInt(256), srand.nextInt(256), srand.nextInt(256)));
+                	pane.getChildren().add(circle);
+                	timeline.getKeyFrames().addAll(
+                        	new KeyFrame(new Duration(300),
+                        	  new KeyValue(circle.translateXProperty(), 0),
+                        	  new KeyValue(circle.translateYProperty(), 55)),
+                		new KeyFrame(new Duration(450),
+                        	  new KeyValue(circle.translateXProperty(), change[0]*9),
+                        	  new KeyValue(circle.translateYProperty(), 85)),
+                        	new KeyFrame(new Duration(600),
+                        	  new KeyValue(circle.translateXProperty(), change[0]*15),
+                        	  new KeyValue(circle.translateYProperty(), 85)),
+                        	new KeyFrame(new Duration(750),
+                        	  new KeyValue(circle.translateXProperty(), change[0]*15+change[1]*9),
+                        	  new KeyValue(circle.translateYProperty(), 85)),
+                		new KeyFrame(new Duration(900),
+                        	  new KeyValue(circle.translateXProperty(), change[0]*15+change[1]*9),
+                        	  new KeyValue(circle.translateYProperty(), 115)),
+                        	new KeyFrame(new Duration(1050),
+                        	  new KeyValue(circle.translateXProperty(), change[0]*15+change[1]*15+change[2]*9),
+                        	  new KeyValue(circle.translateYProperty(), 115)),
+                        	new KeyFrame(new Duration(1200),
+                        	  new KeyValue(circle.translateXProperty(), change[0]*15+change[1]*15+change[2]*15),
+                        	  new KeyValue(circle.translateYProperty(), 145)),
+                        	new KeyFrame(new Duration(1350),
+                        	  new KeyValue(circle.translateXProperty(), change[0]*15+change[1]*15+change[2]*15+change[3]*9),
+                        	  new KeyValue(circle.translateYProperty(), 145)),
+                        	new KeyFrame(new Duration(1500),
+                        	  new KeyValue(circle.translateXProperty(), change[0]*15+change[1]*15+change[2]*15+change[3]*15),
+                        	  new KeyValue(circle.translateYProperty(), 175)),
+                        	new KeyFrame(new Duration(1650),
+                        	  new KeyValue(circle.translateXProperty(), change[0]*15+change[1]*15+change[2]*15+change[3]*15+change[4]*9),
+                        	  new KeyValue(circle.translateYProperty(), 175)),
+                        	new KeyFrame(new Duration(1800),
+                        	  new KeyValue(circle.translateXProperty(), change[0]*15+change[1]*15+change[2]*15+change[3]*15+change[4]*15),
+                        	  new KeyValue(circle.translateYProperty(), 205)),
+                        	new KeyFrame(new Duration(1950),
+                        	  new KeyValue(circle.translateXProperty(), change[0]*15+change[1]*15+change[2]*15+change[3]*15+change[4]*15+change[5]*9),
+                        	  new KeyValue(circle.translateYProperty(), 205)), 
+                        	new KeyFrame(new Duration(2100),
+                        	 new KeyValue(circle.translateXProperty(), change[0]*15+change[1]*15+change[2]*15+change[3]*15+change[4]*15+change[5]*15),
+                        	 new KeyValue(circle.translateYProperty(), 235)),
+                        	new KeyFrame(new Duration(2250),
+                        	  new KeyValue(circle.translateXProperty(), change[0]*15+change[1]*15+change[2]*15+change[3]*15
                                   +change[4]*15+change[5]*15+change[6]*9),
-                          new KeyValue(circle.translateYProperty(), 235)),
-                        new KeyFrame(new Duration(2400),
-                          new KeyValue(circle.translateXProperty(), change[0]*15+change[1]*15+change[2]*15+change[3]*15+change[4]*15+change[5]*15+change[6]*15),
-                          new KeyValue(circle.translateYProperty(), 270)),
-                        new KeyFrame(new Duration(2600),
-                          new KeyValue(circle.translateXProperty(), change[0]*15+change[1]*15+change[2]*15+change[3]*15+change[4]*15+change[5]*15+change[6]*15),
-                          new KeyValue(circle.translateYProperty(), 300))
-                );
+                        	  new KeyValue(circle.translateYProperty(), 235)),
+                        	new KeyFrame(new Duration(2400),
+                        	  new KeyValue(circle.translateXProperty(), change[0]*15+change[1]*15+change[2]*15+change[3]*15+change[4]*15+change[5]*15+change[6]*15),
+                        	  new KeyValue(circle.translateYProperty(), 270)),
+                        	new KeyFrame(new Duration(2600),
+                        	  new KeyValue(circle.translateXProperty(), change[0]*15+change[1]*15+change[2]*15+change[3]*15+change[4]*15+change[5]*15+change[6]*15),
+                        	  new KeyValue(circle.translateYProperty(), 300))
+                	);
+                	
                 switch(change[0]*15+change[1]*15+change[2]*15+change[3]*15+change[4]*15+change[5]*15+change[6]*15) {
                     case -105:
                         eachCounter[0].setText(String.valueOf(++eachCount[0]));
@@ -151,14 +156,16 @@ public class BeanGame extends Application{//U10416038 °¨¹Å¨}
                         eachCounter[7].setText(String.valueOf(++eachCount[7]));
                         break;
                 }
+                
                 count++;
                 counter.setText("You have thrown "+count+" beans");
                 timeline.play();
             }
         });
+        
 	Scene scene = new Scene(pane, 460, 380 , Color.GREY);//Set for stage and scene
-    primaryStage.setResizable(false);
-    primaryStage.setTitle("U10416038   BeanGame");
+    	primaryStage.setResizable(false);
+    	primaryStage.setTitle("U10416038   BeanGame");
 	primaryStage.setScene(scene);
 	primaryStage.show();
 	}
